649be4ff6aafe18ecdbb6edf30e1f20c5ed67cd4
NEO JAN CONCEPCION <97005633+HAIRIGIRL1@users.noreply.github.com>
2026-07-08 01:48:52 +0800

    Create exceptiions..js

---
+++ core/handlers/exceptiions..js
@@ -0,0 +1,11 @@
+export class BalanceError extends Error {
+    constructor(name, message) {
+        super(message);
+        this.name = name;
+    }
+}export class BalanceError extends Error {
+    constructor(name, message) {
+        super(message);
+        this.name = name;
+    }
+}
